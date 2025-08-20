02c4bb395103fc6fe2dce10db623cc9c6b414937
Saku Karttunen <saku.karttunen@gmail.com>
2025-08-20 16:30:30 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  # using zsh, enter the shell using: nix develop --command zsh
+  description = "Development flake for the portfolio project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        propagatedBuildInputs = with pkgs; [
+          dotnetCorePackages.dotnet_9.sdk
+          dotnetCorePackages.dotnet_9.runtime
+        ];
+      };
+    };
+}
